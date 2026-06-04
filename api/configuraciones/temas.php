07f7ea71b52312d2f8f693947aa2c077b866e333
Romario2404 <santosronaldino78@gmail.com>
2026-06-03 20:05:12 -0500

    Delete api directory

--- api/configuraciones/temas.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../../modules/configuraciones/Configuraciones.php';
-
-$config = new Configuraciones();
-jsonResponse($config->getTemas());
--- api/reportes/listar.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../../modules/reportes/Reportes.php';
-
-$user = requireAuth();
-
-$reportes = new Reportes();
-$data = $reportes->listar($user['id']);
-jsonResponse($data);
